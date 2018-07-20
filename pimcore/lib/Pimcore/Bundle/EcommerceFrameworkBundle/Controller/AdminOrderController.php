redcar1995/Shopping-site

commit f173bd7b63f47c997fdb788cfd133fbd54c86589

[Ecommerce] Replace google maps with leaflet on order detail page (#3108)
* Update google maps with leaflet on order detail page

* code optimizations

pimcore/lib/Pimcore/Bundle/EcommerceFrameworkBundle/Controller/AdminOrderController.php
@@ -177,31 +177,45 @@ class AdminOrderController extends AdminController implements EventedControllerI
         /* @var AbstractOrder $order */
         $orderAgent = $this->orderManager->createOrderAgent($order);
 
+        // config
+        $this->view->pimcoreSymfonyConfig = $this->getParameter('pimcore.config');
+
         /**
          * @param array $address
          *
          * @return string
          */
         $geoPoint = function (array $address) {
-            // https://developers.google.com/maps/documentation/geocoding/index?hl=de#JSON
-            $url = sprintf(
-                'http://maps.googleapis.com/maps/api/geocode/json?address=%1$s&sensor=false',
+
+            $baseUrl = $this->view->pimcoreSymfonyConfig['maps']['geocoding_url_template'];
+            $url = str_replace(
+                '{q}',
                 urlencode(
                     $address[0]
                     . ' ' . $address[1]
                     . ' ' . $address[2]
                     . ' ' . $address[3]
-                )
+                ),
+                $baseUrl
             );
-            $json = json_decode(file_get_contents($url));
 
-            return $json->results[0]->geometry->location;
+            $client = \Pimcore::getContainer()->get('pimcore.http_client');
+            $response = $client->request('GET', $url);
+
+            if ($response->getStatusCode() < 300) {
+                $json = json_decode($response->getBody());
+                if (is_array($json)) {
+                    $json = $json[0];
+                }
+            }
+
+            return $json;
         };
 
         // get geo point
-        $geoAddressInvoice = $geoPoint([$order->getCustomerStreet(), $order->getCustomerZip(), $order->getCustomerCity(), $order->getCustomerCountry()]);
+        $this->view->geoAddressInvoice = $geoPoint([$order->getCustomerStreet(), $order->getCustomerZip(), $order->getCustomerCity(), $order->getCustomerCountry()]);
         if ($order->getDeliveryStreet() && $order->getDeliveryZip()) {
-            $geoAddressDelivery = $geoPoint([$order->getDeliveryStreet(), $order->getDeliveryZip(), $order->getDeliveryCity(), $order->getDeliveryCountry()]);
+            $this->view->geoAddressDelivery = $geoPoint([$order->getDeliveryStreet(), $order->getDeliveryZip(), $order->getDeliveryCity(), $order->getDeliveryCountry()]);
         }
 
         // get customer info

pimcore/lib/Pimcore/Bundle/EcommerceFrameworkBundle/Resources/views/AdminOrder/detail.html.php
@@ -21,6 +21,14 @@
 
 $this->extend('PimcoreEcommerceFrameworkBundle::back-office.html.php');
 
+/* Leaflet CSS
+====================== */
+$this->headLink()->appendStylesheet('/pimcore/static6/js/lib/leaflet/leaflet.css');
+
+/* Leaflet Javascript
+====================== */
+$this->headScript()->appendFile('/pimcore/static6/js/lib/leaflet/leaflet.js');
+
 $orderAgent = $this->orderAgent;
 $order = $orderAgent->getOrder();
 $currency = $orderAgent->getCurrency();
@@ -312,6 +320,7 @@ $regionArray = $locale->getDisplayRegions();
                         , $geoPoint->lng
                     );
                     ?>
+
                     <a href="<?= $urlLink ?>" target="_blank" class="pull-right address-map">
                         <img src="<?= $urlImage ?>" alt=""/>
                     </a>
@@ -342,8 +351,11 @@ $regionArray = $locale->getDisplayRegions();
                                 <?php endif; ?>
                             </address>
                         </div>
-
-                        <?= $this->geoAddressInvoice ? $printMap($this->geoAddressInvoice) : '' ?>
+                        <?php if ($this->geoAddressInvoice->lat && $this->geoAddressInvoice->lon) { ?>
+                            <div class="col-md-6">
+                                <div id="leafletmap-invoice" style="width: 200px; height:200px"></div>
+                            </div>
+                        <?php } ?>
                     </div>
                 </div>
 
@@ -368,8 +380,11 @@ $regionArray = $locale->getDisplayRegions();
                                 <?= strtoupper($regionArray[$order->getDeliveryCountry()]) ?><br/>
                             </address>
                         </div>
-
-                        <?= $this->geoAddressDelivery ? $printMap($this->geoAddressDelivery) : '' ?>
+                        <?php if ($this->geoAddressInvoice->lat && $this->geoAddressInvoice->lon) { ?>
+                            <div class="col-md-6">
+                                <div id="leafletmap-delivery" style="width: 200px; height:200px"></div>
+                            </div>
+                        <?php } ?>
                     </div>
                 </div>
                 <?php endif; ?>
@@ -502,6 +517,19 @@ $regionArray = $locale->getDisplayRegions();
             $(this).removeData('bs.modal');
             $(this).find('.modal-content').html("");
         });
+
+        var latitude = "<?=$this->geoAddressInvoice->lat;?>";
+        var longitude = "<?=$this->geoAddressInvoice->lon;?>";
+        var tileLayerUrl = "<?=$this->pimcoreSymfonyConfig['maps']['tile_layer_url_template'];?>";
+
+        if (latitude && longitude) {
+            var leafletMapInvoice =  L.map("leafletmap-invoice").setView([latitude, longitude], 10);
+
+            var leafletMapDelivery =  L.map("leafletmap-delivery").setView([latitude, longitude], 10);
+
+            L.tileLayer(tileLayerUrl).addTo(leafletMapInvoice);
+            L.tileLayer(tileLayerUrl).addTo(leafletMapDelivery);
+        }
 
     });
     <?php $this->headScript()->captureEnd(); ?>

commit 5125c6ea15cbe09b3201a90e12bf197b11823a1c

E-Commerce order detail, fixed Leaflet/OSM issues caused by #3108 - see also #2995

pimcore/lib/Pimcore/Bundle/EcommerceFrameworkBundle/Controller/AdminOrderController.php
@@ -171,22 +171,21 @@ class AdminOrderController extends AdminController implements EventedControllerI
     public function detailAction(Request $request)
     {
         $dateFormatter = $this->get('pimcore.locale.intl_formatter');
+        $pimcoreSymfonyConfig = $this->getParameter('pimcore.config');
 
         // init
         $order = OnlineShopOrder::getById($request->get('id'));
         /* @var AbstractOrder $order */
         $orderAgent = $this->orderManager->createOrderAgent($order);
 
-        // config
-        $this->view->pimcoreSymfonyConfig = $this->getParameter('pimcore.config');
-
         /**
          * @param array $address
          *
          * @return string
          */
-        $geoPoint = function (array $address) {
-            $baseUrl = $this->view->pimcoreSymfonyConfig['maps']['geocoding_url_template'];
+        $geoPoint = function (array $address) use ($pimcoreSymfonyConfig) {
+
+            $baseUrl = $pimcoreSymfonyConfig['maps']['geocoding_url_template'];
             $url = str_replace(
                 '{q}',
                 urlencode(
@@ -198,23 +197,27 @@ class AdminOrderController extends AdminController implements EventedControllerI
                 $baseUrl
             );
 
+            $json = null;
             $client = \Pimcore::getContainer()->get('pimcore.http_client');
-            $response = $client->request('GET', $url);
-
-            if ($response->getStatusCode() < 300) {
-                $json = json_decode($response->getBody());
-                if (is_array($json)) {
-                    $json = $json[0];
+            try {
+                $response = $client->request('GET', $url);
+                if ($response->getStatusCode() < 300) {
+                    $json = json_decode($response->getBody());
+                    if (is_array($json)) {
+                        $json = $json[0];
+                    }
                 }
+            } catch (\Exception $e) {
+                // noting to do
             }
 
             return $json;
         };
 
         // get geo point
-        $this->view->geoAddressInvoice = $geoPoint([$order->getCustomerStreet(), $order->getCustomerZip(), $order->getCustomerCity(), $order->getCustomerCountry()]);
+        $geoAddressInvoice = $geoPoint([$order->getCustomerStreet(), $order->getCustomerZip(), $order->getCustomerCity(), $order->getCustomerCountry()]);
         if ($order->getDeliveryStreet() && $order->getDeliveryZip()) {
-            $this->view->geoAddressDelivery = $geoPoint([$order->getDeliveryStreet(), $order->getDeliveryZip(), $order->getDeliveryCity(), $order->getDeliveryCountry()]);
+            $geoAddressDelivery = $geoPoint([$order->getDeliveryStreet(), $order->getDeliveryZip(), $order->getDeliveryCity(), $order->getDeliveryCountry()]);
         }
 
         // get customer info
@@ -294,7 +297,8 @@ class AdminOrderController extends AdminController implements EventedControllerI
             'timeLine' => $arrTimeline,
             'geoAddressInvoice' => $geoAddressInvoice,
             'arrCustomerAccount' => $arrCustomerAccount,
-            'geoAddressDelivery' => $geoAddressDelivery
+            'geoAddressDelivery' => $geoAddressDelivery,
+            'pimcoreSymfonyConfig' => $pimcoreSymfonyConfig
         ];
     }
 

pimcore/lib/Pimcore/Bundle/EcommerceFrameworkBundle/Resources/views/AdminOrder/detail.html.php
@@ -305,29 +305,6 @@ $regionArray = $locale->getDisplayRegions();
 
             <!-- Tab panes -->
             <div class="tab-content">
-                <?php
-                /**
-                 * print google static map
-                 * @param stdClass $geoPoint
-                 */
-                $printMap = function (stdClass $geoPoint) {
-                    $urlLink = sprintf('http://maps.google.de/maps?q=loc:%1$s,%2$s'
-                        , $geoPoint->lat
-                        , $geoPoint->lng
-                    );
-                    $urlImage = sprintf('http://maps.googleapis.com/maps/api/staticmap?center=%1$s,%2$s&zoom=11&size=200x200&sensor=false'
-                        , $geoPoint->lat
-                        , $geoPoint->lng
-                    );
-                    ?>
-
-                    <a href="<?= $urlLink ?>" target="_blank" class="pull-right address-map">
-                        <img src="<?= $urlImage ?>" alt=""/>
-                    </a>
-                    <?php
-                };
-                ?>
-
                 <div role="tabpanel" class="tab-pane active" id="addressInvoice">
                     <div class="row">
                         <div class="col-md-6">
@@ -351,7 +328,7 @@ $regionArray = $locale->getDisplayRegions();
                                 <?php endif; ?>
                             </address>
                         </div>
-                        <?php if ($this->geoAddressInvoice->lat && $this->geoAddressInvoice->lon) { ?>
+                        <?php if ($this->geoAddressInvoice) { ?>
                             <div class="col-md-6">
                                 <div id="leafletmap-invoice" style="width: 200px; height:200px"></div>
                             </div>
@@ -380,7 +357,7 @@ $regionArray = $locale->getDisplayRegions();
                                 <?= strtoupper($regionArray[$order->getDeliveryCountry()]) ?><br/>
                             </address>
                         </div>
-                        <?php if ($this->geoAddressInvoice->lat && $this->geoAddressInvoice->lon) { ?>
+                        <?php if ($this->geoAddressDelivery) { ?>
                             <div class="col-md-6">
                                 <div id="leafletmap-delivery" style="width: 200px; height:200px"></div>
                             </div>
@@ -511,25 +488,28 @@ $regionArray = $locale->getDisplayRegions();
         // enable popover
         $('[data-toggle="popover"]').popover({html: true});
 
-
         // remove modal on close
         $('body').on('hidden.bs.modal', '.modal', function () {
             $(this).removeData('bs.modal');
             $(this).find('.modal-content').html("");
         });
 
-        var latitude = "<?=$this->geoAddressInvoice->lat;?>";
-        var longitude = "<?=$this->geoAddressInvoice->lon;?>";
         var tileLayerUrl = "<?=$this->pimcoreSymfonyConfig['maps']['tile_layer_url_template'];?>";
-
-        if (latitude && longitude) {
-            var leafletMapInvoice =  L.map("leafletmap-invoice").setView([latitude, longitude], 10);
-
-            var leafletMapDelivery =  L.map("leafletmap-delivery").setView([latitude, longitude], 10);
-
+        <?php if($this->geoAddressInvoice) { ?>
+            var leafletMapInvoice =  L.map("leafletmap-invoice").setView([<?= $this->geoAddressInvoice->lat ?>, <?= $this->geoAddressInvoice->lon ?>], 10);
             L.tileLayer(tileLayerUrl).addTo(leafletMapInvoice);
+            L.marker([<?= $this->geoAddressInvoice->lat ?>, <?= $this->geoAddressInvoice->lon ?>]).addTo(leafletMapInvoice);
+        <?php } ?>
+
+        <?php if($this->geoAddressDelivery) { ?>
+            var leafletMapDelivery =  L.map("leafletmap-delivery").setView([<?= $this->geoAddressDelivery->lat ?>, <?= $this->geoAddressDelivery->lon ?>], 10);
             L.tileLayer(tileLayerUrl).addTo(leafletMapDelivery);
-        }
+            L.marker([<?= $this->geoAddressDelivery->lat ?>, <?= $this->geoAddressDelivery->lon ?>]).addTo(leafletMapDelivery);
+
+            $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+                leafletMapDelivery.invalidateSize();
+            });
+        <?php } ?>
 
     });
     <?php $this->headScript()->captureEnd(); ?>