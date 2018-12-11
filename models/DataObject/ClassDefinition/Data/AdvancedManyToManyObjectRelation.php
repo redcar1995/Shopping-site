redcar1995/Shopping-site

commit d5d4a8fc883a47835e6580dcf500ddf21f99195f

Interfaces for Data Types (#3715)

models/DataObject/ClassDefinition/Data/AdvancedManyToManyObjectRelation.php
@@ -21,7 +21,7 @@ use Pimcore\Model;
 use Pimcore\Model\DataObject;
 use Pimcore\Model\Element;
 
-class AdvancedManyToManyObjectRelation extends Model\DataObject\ClassDefinition\Data\ManyToManyObjectRelation
+class AdvancedManyToManyObjectRelation extends ManyToManyObjectRelation implements CustomResourcePersistingInterface, QueryResourcePersistenceAwareInterface
 {
     use DataObject\Traits\ElementWithMetadataComparisonTrait;
 
@@ -143,7 +143,6 @@ class AdvancedManyToManyObjectRelation extends Model\DataObject\ClassDefinition\
      */
     public function getDataForQueryResource($data, $object = null, $params = [])
     {
-
         //return null when data is not set
         if (!$data) {
             return null;

models/DataObject/ClassDefinition/Data/AdvancedManyToManyRelation.php
@@ -24,7 +24,7 @@ use Pimcore\Model\DataObject;
 use Pimcore\Model\Document;
 use Pimcore\Model\Element;
 
-class AdvancedManyToManyRelation extends Model\DataObject\ClassDefinition\Data\ManyToManyRelation
+class AdvancedManyToManyRelation extends ManyToManyObjectRelation implements CustomResourcePersistingInterface, QueryResourcePersistenceAwareInterface
 {
     use DataObject\Traits\ElementWithMetadataComparisonTrait;
     /**

models/DataObject/ClassDefinition/Data/Relations/AbstractRelations.php
@@ -20,9 +20,11 @@ use Pimcore\Db;
 use Pimcore\Logger;
 use Pimcore\Model;
 use Pimcore\Model\DataObject;
+use Pimcore\Model\DataObject\ClassDefinition\Data;
+use Pimcore\Model\DataObject\ClassDefinition\Data\CustomResourcePersistingInterface;
 use Pimcore\Model\Element;
 
-abstract class AbstractRelations extends Model\DataObject\ClassDefinition\Data
+abstract class AbstractRelations extends Data implements CustomResourcePersistingInterface
 {
     /**
      * @var bool
@@ -386,6 +388,14 @@ abstract class AbstractRelations extends Model\DataObject\ClassDefinition\Data
         }
 
         return $data;
+    }
+
+    /**
+     * @param $object
+     * @param array $params
+     */
+    public function delete($object, $params = [])
+    {
     }
 
     /**

lib/Tool.php
@@ -775,6 +775,24 @@ class Tool
     }
 
     /**
+     * @param string $class
+     * @param string $method
+     * @param string $interface
+     */
+    public static function triggerDeprecatedMethodWarning($class, $method, $interface)
+    {
+        @trigger_error(
+            sprintf(
+                '%s use method %s, but hasn\'t a %s interface. This won\'t work in v6.0.',
+                $class,
+                $method,
+                $interface
+            ),
+            E_USER_DEPRECATED
+        );
+    }
+
+    /**
      * @param $name
      * @param $arguments
      *

models/DataObject/ClassDefinition/Data.php
@@ -130,36 +130,6 @@ abstract class Data
     ];
 
     /**
-     * Returns the the data that should be stored in the resource
-     *
-     * @param mixed $data
-     *
-     * @return mixed
-     *
-     * abstract public function getDataForResource($data);
-     */
-
-    /**
-     * Convert the saved data in the resource to the internal eg. Image-Id to Asset\Image object, this is the inverted getDataForResource()
-     *
-     * @param mixed $data
-     *
-     * @return mixed
-     *
-     * abstract public function getDataFromResource($data);
-     */
-
-    /**
-     * Returns the data which should be stored in the query columns
-     *
-     * @param mixed $data
-     *
-     * @return mixed
-     *
-     * abstract public function getDataForQueryResource($data);
-     */
-
-    /**
      * Returns the data for the editmode
      *
      * @param mixed $data

models/DataObject/ClassDefinition/Data/Block.php
@@ -20,10 +20,11 @@ use Pimcore\Db;
 use Pimcore\Logger;
 use Pimcore\Model;
 use Pimcore\Model\DataObject;
+use Pimcore\Model\DataObject\ClassDefinition\Data;
 use Pimcore\Model\Element;
 use Pimcore\Tool\Serialize;
 
-class Block extends Model\DataObject\ClassDefinition\Data
+class Block extends Data implements CustomResourcePersistingInterface, ResourcePersistenceAwareInterface
 {
     use Element\ChildsCompatibilityTrait;
 
@@ -224,20 +225,6 @@ class Block extends Model\DataObject\ClassDefinition\Data
             return $result;
         }
 
-        return null;
-    }
-
-    /**
-     * @see DataObject\ClassDefinition\Data::getDataForQueryResource
-     *
-     * @param string $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return string
-     */
-    public function getDataForQueryResource($data, $object = null, $params = [])
-    {
         return null;
     }
 
@@ -907,6 +894,14 @@ class Block extends Model\DataObject\ClassDefinition\Data
     /**
      * @param $object
      * @param array $params
+     */
+    public function save($object, $params = [])
+    {
+    }
+
+    /**
+     * @param $object
+     * @param array $params
      *
      * @return null
      */
@@ -962,6 +957,14 @@ class Block extends Model\DataObject\ClassDefinition\Data
         }
 
         return $data;
+    }
+
+    /**
+     * @param $object
+     * @param array $params
+     */
+    public function delete($object, $params = [])
+    {
     }
 
     /**

models/DataObject/ClassDefinition/Data/BooleanSelect.php
@@ -18,8 +18,9 @@ namespace Pimcore\Model\DataObject\ClassDefinition\Data;
 
 use Pimcore\Model;
 use Pimcore\Model\DataObject;
+use Pimcore\Model\DataObject\ClassDefinition\Data;
 
-class BooleanSelect extends Model\DataObject\ClassDefinition\Data
+class BooleanSelect extends Data implements ResourcePersistenceAwareInterface
 {
     use Model\DataObject\Traits\SimpleComparisonTrait;
 

models/DataObject/ClassDefinition/Data/CalculatedValue.php
@@ -18,8 +18,9 @@ namespace Pimcore\Model\DataObject\ClassDefinition\Data;
 
 use Pimcore\Model;
 use Pimcore\Model\DataObject;
+use Pimcore\Model\DataObject\ClassDefinition\Data;
 
-class CalculatedValue extends Model\DataObject\ClassDefinition\Data
+class CalculatedValue extends Data implements QueryResourcePersistenceAwareInterface
 {
     /**
      * Static type of this element
@@ -111,34 +112,6 @@ class CalculatedValue extends Model\DataObject\ClassDefinition\Data
     public function setCalculatorClass($calculatorClass)
     {
         $this->calculatorClass = $calculatorClass;
-    }
-
-    /**
-     * @see Object_Class_Data::getDataForResource
-     *
-     * @param float $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return float
-     */
-    public function getDataForResource($data, $object = null, $params = [])
-    {
-        // nothing to do
-    }
-
-    /**
-     * @see Object_Class_Data::getDataFromResource
-     *
-     * @param float $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return float
-     */
-    public function getDataFromResource($data, $object = null, $params = [])
-    {
-        // nothing to do
     }
 
     /**
@@ -288,15 +261,6 @@ class CalculatedValue extends Model\DataObject\ClassDefinition\Data
     public function getColumnType()
     {
         return null;
-    }
-
-    public function save()
-    {
-        // nothing to do
-    }
-
-    public function load()
-    {
     }
 
     /**

commit 44338081dfed41aae416905bb36fd8c7420d27c1

[Objects] Relation Data-Types shouldn't use methods getDataForResource() and getDataFromResource() - fixes issues caused by #3715

models/DataObject/ClassDefinition/Data/AdvancedManyToManyObjectRelation.php
@@ -55,15 +55,9 @@ class AdvancedManyToManyObjectRelation extends ManyToManyObjectRelation implemen
     public $phpdocType = '\\Pimcore\\Model\\DataObject\\Data\\ObjectMetadata[]';
 
     /**
-     * @see ResourcePersistenceAwareInterface::getDataForResource
-     *
-     * @param array $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return array
+     * @inheritdoc
      */
-    public function getDataForResource($data, $object = null, $params = [])
+    public function prepareDataForPersistence($data, $object = null, $params = [])
     {
         $return = [];
 
@@ -93,15 +87,9 @@ class AdvancedManyToManyObjectRelation extends ManyToManyObjectRelation implemen
     }
 
     /**
-     * @see ResourcePersistenceAwareInterface::getDataFromResource
-     *
-     * @param array $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return array
+     * @inheritdoc
      */
-    public function getDataFromResource($data, $object = null, $params = [])
+    public function loadData($data, $object = null, $params = [])
     {
         $objects = [];
 

models/DataObject/ClassDefinition/Data/AdvancedManyToManyRelation.php
@@ -47,15 +47,9 @@ class AdvancedManyToManyRelation extends ManyToManyObjectRelation implements Cus
     public $phpdocType = '\\Pimcore\\Model\\DataObject\\Data\\ElementMetadata[]';
 
     /**
-     * @see ResourcePersistenceAwareInterface::getDataForResource
-     *
-     * @param array $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return array
+     * @inheritdoc
      */
-    public function getDataForResource($data, $object = null, $params = [])
+    public function prepareDataForPersistence($data, $object = null, $params = [])
     {
         $return = [];
 
@@ -85,15 +79,9 @@ class AdvancedManyToManyRelation extends ManyToManyObjectRelation implements Cus
     }
 
     /**
-     * @see ResourcePersistenceAwareInterface::getDataFromResource
-     *
-     * @param array $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return array
+     * @inheritdoc
      */
-    public function getDataFromResource($data, $object = null, $params = [])
+    public function loadData($data, $object = null, $params = [])
     {
         $list = [];
 

models/DataObject/ClassDefinition/Data/ManyToManyObjectRelation.php
@@ -82,15 +82,9 @@ class ManyToManyObjectRelation extends AbstractRelations implements QueryResourc
     }
 
     /**
-     * @see ResourcePersistenceAwareInterface::getDataForResource
-     *
-     * @param array $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return array
+     * @inheritdoc
      */
-    public function getDataForResource($data, $object = null, $params = [])
+    public function prepareDataForPersistence($data, $object = null, $params = [])
     {
         $return = [];
 
@@ -119,15 +113,9 @@ class ManyToManyObjectRelation extends AbstractRelations implements QueryResourc
     }
 
     /**
-     * @see ResourcePersistenceAwareInterface::getDataFromResource
-     *
-     * @param array $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return array
+     * @inheritdoc
      */
-    public function getDataFromResource($data, $object = null, $params = [])
+    public function loadData($data, $object = null, $params = [])
     {
         $objects = [];
         if (is_array($data) && count($data) > 0) {

models/DataObject/ClassDefinition/Data/ManyToManyRelation.php
@@ -210,16 +210,9 @@ class ManyToManyRelation extends AbstractRelations implements QueryResourcePersi
     }
 
     /**
-     * @see ResourcePersistenceAwareInterface::getDataForResource
-     *
-     * @param array $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     * @param mixed $params
-     *
-     * @return array
+     * @inheritdoc
      */
-    public function getDataForResource($data, $object = null, $params = [])
+    public function prepareDataForPersistence($data, $object = null, $params = [])
     {
         $return = [];
 
@@ -248,15 +241,9 @@ class ManyToManyRelation extends AbstractRelations implements QueryResourcePersi
     }
 
     /**
-     * @see ResourcePersistenceAwareInterface::getDataFromResource
-     *
-     * @param array $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return array
+     * @inheritdoc
      */
-    public function getDataFromResource($data, $object = null, $params = [])
+    public function loadData($data, $object = null, $params = [])
     {
         $elements = [];
         if (is_array($data) && count($data) > 0) {

models/DataObject/ClassDefinition/Data/ManyToOneRelation.php
@@ -202,15 +202,9 @@ class ManyToOneRelation extends AbstractRelations implements QueryResourcePersis
     }
 
     /**
-     * @see ResourcePersistenceAwareInterface::getDataForResource
-     *
-     * @param Asset | Document | DataObject\AbstractObject $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     *
-     * @return array
+     * @inheritdoc
      */
-    public function getDataForResource($data, $object = null, $params = [])
+    public function prepareDataForPersistence($data, $object = null, $params = [])
     {
         if ($data instanceof Element\ElementInterface) {
             $type = Element\Service::getType($data);
@@ -227,16 +221,9 @@ class ManyToOneRelation extends AbstractRelations implements QueryResourcePersis
     }
 
     /**
-     * @see ResourcePersistenceAwareInterface::getDataFromResource
-     *
-     * @param array $data
-     * @param null|Model\DataObject\AbstractObject $object
-     * @param mixed $params
-     * @param bool $notRelationTable
-     *
-     * @return Asset|Document|DataObject\AbstractObject
+     * @inheritdoc
      */
-    public function getDataFromResource($data, $object = null, $params = [], $notRelationTable = false)
+    public function loadData($data, $object = null, $params = [])
     {
         // data from relation table
         $data = is_array($data) ? $data : [];
@@ -260,7 +247,7 @@ class ManyToOneRelation extends AbstractRelations implements QueryResourcePersis
      */
     public function getDataForQueryResource($data, $object = null, $params = [])
     {
-        $rData = $this->getDataForResource($data, $object, $params);
+        $rData = $this->prepareDataForPersistence($data, $object, $params);
 
         $return = [];
         $return[$this->getName() . '__id'] = $rData[0]['dest_id'];

models/DataObject/ClassDefinition/Data/Relations/AbstractRelations.php
@@ -303,7 +303,7 @@ abstract class AbstractRelations extends Data implements CustomResourcePersistin
         $db = Db::get();
 
         $data = $this->getDataFromObjectParam($object, $params);
-        $relations = $this->getDataForResource($data, $object, $params);
+        $relations = $this->prepareDataForPersistence($data, $object, $params);
 
         if (is_array($relations) && !empty($relations)) {
             foreach ($relations as $relation) {
@@ -382,13 +382,29 @@ abstract class AbstractRelations extends Data implements CustomResourcePersistin
             return ($a['index'] < $b['index']) ? -1 : 1;
         });
 
-        $data = $this->getDataFromResource($relations, $object, $params);
+        $data = $this->loadData($relations, $object, $params);
         if ($object instanceof DataObject\DirtyIndicatorInterface) {
             $object->markFieldDirty($this->getName(), false);
         }
 
         return $data;
     }
+
+    /**
+     * @param array $data
+     * @param DataObject\Concrete $object
+     * @param array $params
+     * @return mixed
+     */
+    abstract public function loadData($data, $object = null, $params = []);
+
+    /**
+     * @param array $data
+     * @param DataObject\Concrete $object
+     * @param array $params
+     * @return mixed
+     */
+    abstract public function prepareDataForPersistence($data, $object = null, $params = []);
 
     /**
      * @param $object