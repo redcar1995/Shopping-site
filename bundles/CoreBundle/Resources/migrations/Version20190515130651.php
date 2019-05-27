redcar1995/Shopping-site

commit 2a0f906943d4dd2880992a06668cfef04f0ea92f

asset_metadata add primary key (#4375)
* add primary key to asset_metadata table

* add primary key to asset_metadata table - keep cid index

bundles/CoreBundle/Resources/migrations/Version20190515130651.php
@@ -1,0 +1,31 @@
+<?php
+
+namespace Pimcore\Bundle\CoreBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
+
+class Version20190515130651 extends AbstractPimcoreMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql('
+            ALTER TABLE `assets_metadata` CHANGE `language` `language` varchar(10) COLLATE \'ascii_general_ci\' NULL AFTER `cid`;
+            ALTER TABLE `assets_metadata` ADD PRIMARY KEY `cid_name_language` (`cid`, `name`, `language`);
+        ');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql('
+            ALTER TABLE `assets_metadata` DROP INDEX `PRIMARY`;
+            ALTER TABLE `assets_metadata` CHANGE `language` `language` varchar(190) COLLATE \'utf8mb4_general_ci\' NOT NULL AFTER `cid`;
+        ');
+    }
+}

bundles/InstallBundle/Resources/install.sql
@@ -49,11 +49,12 @@ DROP TABLE IF EXISTS `assets_metadata`;
 CREATE TABLE `assets_metadata` (
   `cid` int(11) DEFAULT NULL,
   `name` varchar(190) DEFAULT NULL,
-  `language` varchar(190) DEFAULT NULL,
+  `language` varchar(10) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL DEFAULT '',
   `type` ENUM('input','textarea','asset','document','object','date','select','checkbox') DEFAULT NULL,
   `data` text,
-  KEY `cid` (`cid`),
-	INDEX `name` (`name`)
+  PRIMARY KEY (`cid`, `name`, `language`),
+	INDEX `name` (`name`),
+	INDEX `cid` (`cid`)
 ) DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `cache`;

commit e18462608a9cffb11cfc57efd8a90dff92772b04

Fixed migration script and bug caused by #4375 - follow up to #4438

bundles/CoreBundle/Resources/migrations/Version20190515130651.php
@@ -12,10 +12,10 @@ class Version20190515130651 extends AbstractPimcoreMigration
      */
     public function up(Schema $schema)
     {
-        $this->addSql('
-            ALTER TABLE `assets_metadata` CHANGE `language` `language` varchar(10) COLLATE \'ascii_general_ci\' NULL AFTER `cid`;
-            ALTER TABLE `assets_metadata` ADD PRIMARY KEY `cid_name_language` (`cid`, `name`, `language`);
-        ');
+        $this->addSql('ALTER TABLE `assets_metadata` CHANGE `cid` `cid` int(11) NOT NULL;');
+        $this->addSql('ALTER TABLE `assets_metadata` CHANGE `name` `name` varchar(190) NOT NULL;');
+        $this->addSql('ALTER TABLE `assets_metadata` CHANGE `language` `language` varchar(10) COLLATE \'ascii_general_ci\' NULL AFTER `cid`;');
+        $this->addSql('ALTER TABLE `assets_metadata` ADD PRIMARY KEY `cid_name_language` (`cid`, `name`, `language`);');
     }
 
     /**
@@ -23,9 +23,9 @@ class Version20190515130651 extends AbstractPimcoreMigration
      */
     public function down(Schema $schema)
     {
-        $this->addSql('
-            ALTER TABLE `assets_metadata` DROP INDEX `PRIMARY`;
-            ALTER TABLE `assets_metadata` CHANGE `language` `language` varchar(190) COLLATE \'utf8mb4_general_ci\' NOT NULL AFTER `cid`;
-        ');
+        $this->addSql('ALTER TABLE `assets_metadata` DROP INDEX `PRIMARY`;');
+        $this->addSql('ALTER TABLE `assets_metadata` CHANGE `language` `language` varchar(190) COLLATE \'utf8mb4_general_ci\' NOT NULL AFTER `cid`;');
+        $this->addSql('ALTER TABLE `assets_metadata` CHANGE `cid` `cid` int(11) DEFAULT NULL;');
+        $this->addSql('ALTER TABLE `assets_metadata` CHANGE `name` `name` varchar(190) DEFAULT NULL;');
     }
 }

models/Asset/Dao.php
@@ -131,6 +131,8 @@ class Dao extends Model\Element\Dao
                         $metadataItem['data'] = $metadataItem['data']->getId();
                     }
 
+                    $metadataItem['language'] = (string) $metadataItem['language']; // language column cannot be NULL -> see SQL schema
+
                     if (is_scalar($metadataItem['data']) && strlen($metadataItem['data']) > 0) {
                         $this->db->insert('assets_metadata', $metadataItem);
                         $data['hasMetaData'] = 1;