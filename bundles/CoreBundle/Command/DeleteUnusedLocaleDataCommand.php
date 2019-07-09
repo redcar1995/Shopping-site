pimcore/pimcore

commit 8c41e8155805b16dc2d6c6aecb77e2ad871f452c

Run Pimcore\Cache::shutdown() even in CLI, correct handling of write queue in CLI is done in the cache handler internally - fixes #4555

lib/Pimcore.php
@@ -304,9 +304,7 @@ class Pimcore
         self::$inShutdown = true;
 
         // write and clean up cache
-        if (php_sapi_name() != 'cli') {
-            Cache::shutdown();
-        }
+        Cache::shutdown();
 
         // release all open locks from this process
         Model\Tool\Lock::releaseAll();

redcar1995/Shopping-site

commit 4132815c412b1aa4719c7d649214b501dd8bc893

Fixed pre-installation issues, caused by 8c41e8155805b16dc2d6c6aecb77e2ad871f452c (#4555) and 427725f762556bd8763e4c413b46acd3b8c4f7e7 (#4612)

bundles/CoreBundle/Command/DeleteUnusedLocaleDataCommand.php
@@ -16,7 +16,7 @@ namespace Pimcore\Bundle\CoreBundle\Command;
 
 use Pimcore\Console\AbstractCommand;
 use Pimcore\Console\Traits\DryRun;
-use Pimcore\Db\ConnectionInterface;
+use Pimcore\Db;
 use Pimcore\Tool;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -24,22 +24,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class DeleteUnusedLocaleDataCommand extends AbstractCommand
 {
-    /**
-     * @var ConnectionInterface
-     */
-    private $db;
-
     use DryRun;
-
-    /**
-     * @param ConnectionInterface $db
-     */
-    public function __construct(ConnectionInterface $db)
-    {
-        parent::__construct();
-
-        $this->db = $db;
-    }
 
     protected function configure()
     {
@@ -62,6 +47,7 @@ class DeleteUnusedLocaleDataCommand extends AbstractCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $db = Db::get();
         $skipLocales = [];
         if ($input->getOption('skip-locales')) {
             $skipLocales = explode(',', $input->getOption('skip-locales'));
@@ -70,28 +56,28 @@ class DeleteUnusedLocaleDataCommand extends AbstractCommand
         $languageList = [];
         $validLanguages = Tool::getValidLanguages();
         foreach ($validLanguages as $language) {
-            $languageList[] = $this->db->quote($language);
+            $languageList[] = $db->quote($language);
         }
 
-        $tables = $this->db->fetchAll("SHOW TABLES LIKE '%object_localized_data_%'");
+        $tables = $db->fetchAll("SHOW TABLES LIKE '%object_localized_data_%'");
 
         foreach ($tables as $table) {
             $printLine = false;
             $table = current($table);
             $classId = str_replace('object_localized_data_', '', $table);
 
-            $result = $this->db->fetchAll('SELECT DISTINCT `language` FROM ' . $table . ' WHERE `language` NOT IN(' . implode(',', $languageList) .')');
+            $result = $db->fetchAll('SELECT DISTINCT `language` FROM ' . $table . ' WHERE `language` NOT IN(' . implode(',', $languageList) .')');
             $result = ($result ? $result : []);
 
             //delete data from object_localized_data_classID tables
             foreach ($result as $res) {
                 $language = $res['language'];
                 if (!in_array($language, $skipLocales) && !in_array($language, $validLanguages)) {
-                    $sqlDeleteData = 'Delete FROM object_localized_data_' . $classId  . ' WHERE `language` = ' . $this->db->quote($language);
+                    $sqlDeleteData = 'Delete FROM object_localized_data_' . $classId  . ' WHERE `language` = ' . $db->quote($language);
                     $printLine = true;
                     if (!$this->isDryRun()) {
                         $output->writeln($sqlDeleteData);
-                        $this->db->query($sqlDeleteData);
+                        $db->query($sqlDeleteData);
                     } else {
                         $output->writeln($this->dryRunMessage($sqlDeleteData));
                     }
@@ -99,7 +85,7 @@ class DeleteUnusedLocaleDataCommand extends AbstractCommand
             }
 
             //drop unused localized view e.g. object_localized_classId_*
-            $existingViews = $this->db->fetchAll("SHOW TABLES LIKE '%object_localized_{$classId}%'");
+            $existingViews = $db->fetchAll("SHOW TABLES LIKE '%object_localized_{$classId}%'");
 
             if (is_array($existingViews)) {
                 foreach ($existingViews as $existingView) {
@@ -112,7 +98,7 @@ class DeleteUnusedLocaleDataCommand extends AbstractCommand
 
                         if (!$this->isDryRun()) {
                             $output->writeln($sqlDropView);
-                            $this->db->query($sqlDropView);
+                            $db->query($sqlDropView);
                         } else {
                             $output->writeln($this->dryRunMessage($sqlDropView));
                         }
@@ -121,7 +107,7 @@ class DeleteUnusedLocaleDataCommand extends AbstractCommand
             }
 
             //drop unused localized table e.g. object_localized_query_classId_*
-            $existingTables = $this->db->fetchAll("SHOW TABLES LIKE '%object_localized_query_{$classId}%'");
+            $existingTables = $db->fetchAll("SHOW TABLES LIKE '%object_localized_query_{$classId}%'");
             if (is_array($existingTables)) {
                 foreach ($existingTables as $existingTable) {
                     $localizedTable = current($existingTable);
@@ -133,7 +119,7 @@ class DeleteUnusedLocaleDataCommand extends AbstractCommand
 
                         if (!$this->isDryRun()) {
                             $output->writeln($sqlDropTable);
-                            $this->db->query($sqlDropTable);
+                            $db->query($sqlDropTable);
                         } else {
                             $output->writeln($this->dryRunMessage($sqlDropTable));
                         }

lib/Pimcore.php
@@ -303,11 +303,13 @@ class Pimcore
         // set inShutdown to true so that the output-buffer knows that he is allowed to send the headers
         self::$inShutdown = true;
 
-        // write and clean up cache
-        Cache::shutdown();
+        if(self::isInstalled()) {
+            // write and clean up cache
+            Cache::shutdown();
 
-        // release all open locks from this process
-        Model\Tool\Lock::releaseAll();
+            // release all open locks from this process
+            Model\Tool\Lock::releaseAll();
+        }
     }
 
     public static function disableMinifyJs(): bool