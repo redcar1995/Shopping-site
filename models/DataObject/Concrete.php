pimcore/pimcore

commit 5de1954cf87b730beaa1c5e289a547637b8c8385

[Data Object]  array_unique for ManyToMany relations is not working in every use-case - fixes #4958

models/DataObject/Concrete.php
@@ -122,10 +122,17 @@ class Concrete extends AbstractObject implements LazyLoadedFieldsInterface
 
                     $value = $this->$getter();
 
-                    if (is_array($value) and ($fd instanceof ClassDefinition\Data\ManyToManyRelation or $fd instanceof ClassDefinition\Data\ManyToManyObjectRelation)) {
+                    if (is_array($value) && ($fd instanceof ClassDefinition\Data\ManyToManyRelation || $fd instanceof ClassDefinition\Data\ManyToManyObjectRelation)) {
                         //don't save relations twice, if multiple assignments not allowed
                         if (!method_exists($fd, 'getAllowMultipleAssignments') || !$fd->getAllowMultipleAssignments()) {
-                            $value = array_unique($value);
+                            $relationItems = [];
+                            foreach($value as $item) {
+                                if($item instanceof Model\Element\ElementInterface) {
+                                    $relationItems[Model\Element\Service::getElementType($item) . $item->getId()] = $item;
+                                }
+                            }
+
+                            $value = array_values($relationItems);
                         }
                         $this->$setter($value);
                     }
@@ -363,7 +370,7 @@ class Concrete extends AbstractObject implements LazyLoadedFieldsInterface
         $tags['class_' . $this->getClassId()] = 'class_' . $this->getClassId();
         foreach ($this->getClass()->getFieldDefinitions() as $name => $def) {
             // no need to add lazy-loading fields to the cache tags
-            if (!method_exists($def, 'getLazyLoading') or !$def->getLazyLoading()) {
+            if (!method_exists($def, 'getLazyLoading') || !$def->getLazyLoading()) {
                 $tags = $def->getCacheTags($this->getValueForFieldName($name), $tags);
             }
         }

redcar1995/Shopping-site

commit 55a3fb57d5adebf9fc49b00a434d79816768d327

[Data Object] fixed regression caused by 5de1954cf87b730beaa1c5e289a547637b8c8385, see #4958

models/DataObject/Concrete.php
@@ -127,8 +127,17 @@ class Concrete extends AbstractObject implements LazyLoadedFieldsInterface
                         if (!method_exists($fd, 'getAllowMultipleAssignments') || !$fd->getAllowMultipleAssignments()) {
                             $relationItems = [];
                             foreach($value as $item) {
-                                if($item instanceof Model\Element\ElementInterface) {
-                                    $relationItems[Model\Element\Service::getElementType($item) . $item->getId()] = $item;
+                                $elementHash = null;
+                                if($item instanceof Model\DataObject\Data\ObjectMetadata || $item instanceof Model\DataObject\Data\ElementMetadata) {
+                                    if($item->getElement() instanceof Model\Element\ElementInterface) {
+                                        $elementHash = Model\Element\Service::getElementHash($item->getElement());
+                                    }
+                                } elseif($item instanceof Model\Element\ElementInterface) {
+                                    $elementHash = Model\Element\Service::getElementHash($item);
+                                }
+
+                                if($elementHash) {
+                                    $relationItems[$elementHash] = $item;
                                 }
                             }
 

models/Element/Service.php
@@ -463,6 +463,14 @@ class Service extends Model\AbstractModel
     }
 
     /**
+     * @param ElementInterface $element
+     * @return string
+     */
+    public static function getElementHash(ElementInterface $element) : string {
+        return self::getElementType($element) . "-" . $element->getId();
+    }
+
+    /**
      * determines the type of an element (object,asset,document)
      *
      * @static