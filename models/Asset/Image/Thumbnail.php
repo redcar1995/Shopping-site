pimcore/pimcore

commit 64961667dd93cb64f9735a4b8c0fef998b851f83

Fixed problem with "auto" format and WebP format in thumbnail generation - fixes #5637

models/Asset/Image/Thumbnail.php
@@ -319,6 +319,7 @@ class Thumbnail
             // mobile first => fallback image is the smallest possible image
             $fallBackImageThumb = null;
             $isAutoFormat = strtolower($this->getConfig()->getFormat()) === 'source' ? true : false;
+            $webpSupportBackup = null;
 
             if ($isAutoFormat) {
                 $webpSupportBackup = Image\Thumbnail\Processor::setHasWebpSupport(false);
@@ -397,7 +398,7 @@ class Thumbnail
 
             $htmlImgTag = $html;
 
-            if (isset($webpSupportBackup)) {
+            if (isset($isAutoFormat)) {
                 Image\Thumbnail\Processor::setHasWebpSupport($webpSupportBackup);
             }
         }

redcar1995/Shopping-site

commit 7ba873bf8b480eada7008d4e35edabec47d52d20

fixed issue introduced by 64961667dd93cb64f9735a4b8c0fef998b851f83

models/Asset/Image/Thumbnail.php
@@ -398,7 +398,7 @@ class Thumbnail
 
             $htmlImgTag = $html;
 
-            if (isset($isAutoFormat)) {
+            if ($isAutoFormat) {
                 Image\Thumbnail\Processor::setHasWebpSupport($webpSupportBackup);
             }
         }