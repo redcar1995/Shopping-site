pimcore/pimcore

commit 4714c71958904ebd7b9209574856d2c6c8e8763c

Moved AdminStyleTrait to Pimcore\Bundle\AdminBundle\Controller\Traits\ namespace

bundles/AdminBundle/Controller/Traits/AdminStyleTrait.php
@@ -15,7 +15,7 @@
  * @license    http://www.pimcore.org/license     GPLv3 and PEL
  */
 
-namespace Pimcore\Model\Element;
+namespace Pimcore\Bundle\AdminBundle\Controller\Traits;
 
 trait AdminStyleTrait
 {

bundles/AdminBundle/Controller/Admin/AssetController.php
@@ -14,6 +14,7 @@
 
 namespace Pimcore\Bundle\AdminBundle\Controller\Admin;
 
+use Pimcore\Bundle\AdminBundle\Controller\Traits\AdminStyleTrait;
 use Pimcore\Bundle\AdminBundle\Helper\GridHelperService;
 use Pimcore\Controller\Configuration\TemplatePhp;
 use Pimcore\Controller\EventedControllerInterface;
@@ -45,7 +46,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AssetController extends ElementControllerBase implements EventedControllerInterface
 {
-    use Element\AdminStyleTrait;
+    use AdminStyleTrait;
     use ElementEditLockHelperTrait;
 
     /**

bundles/AdminBundle/Controller/Admin/DataObject/DataObjectController.php
@@ -15,6 +15,7 @@
 namespace Pimcore\Bundle\AdminBundle\Controller\Admin\DataObject;
 
 use Pimcore\Bundle\AdminBundle\Controller\Admin\ElementControllerBase;
+use Pimcore\Bundle\AdminBundle\Controller\Traits\AdminStyleTrait;
 use Pimcore\Bundle\AdminBundle\Helper\GridHelperService;
 use Pimcore\Controller\Configuration\TemplatePhp;
 use Pimcore\Controller\EventedControllerInterface;
@@ -46,7 +47,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class DataObjectController extends ElementControllerBase implements EventedControllerInterface
 {
-    use Element\AdminStyleTrait;
+    use AdminStyleTrait;
     use ElementEditLockHelperTrait;
 
     /**

bundles/AdminBundle/Controller/Admin/Document/DocumentController.php
@@ -15,6 +15,7 @@
 namespace Pimcore\Bundle\AdminBundle\Controller\Admin\Document;
 
 use Pimcore\Bundle\AdminBundle\Controller\Admin\ElementControllerBase;
+use Pimcore\Bundle\AdminBundle\Controller\Traits\AdminStyleTrait;
 use Pimcore\Config;
 use Pimcore\Controller\EventedControllerInterface;
 use Pimcore\Db;
@@ -23,7 +24,6 @@ use Pimcore\Event\AdminEvents;
 use Pimcore\Image\HtmlToImage;
 use Pimcore\Logger;
 use Pimcore\Model\Document;
-use Pimcore\Model\Element\AdminStyleTrait;
 use Pimcore\Model\Site;
 use Pimcore\Model\Version;
 use Pimcore\Routing\Dynamic\DocumentRouteHandler;

bundles/AdminBundle/Controller/Admin/Document/DocumentControllerBase.php
@@ -15,6 +15,7 @@
 namespace Pimcore\Bundle\AdminBundle\Controller\Admin\Document;
 
 use Pimcore\Bundle\AdminBundle\Controller\AdminController;
+use Pimcore\Bundle\AdminBundle\Controller\Traits\AdminStyleTrait;
 use Pimcore\Controller\EventedControllerInterface;
 use Pimcore\Logger;
 use Pimcore\Model;
@@ -30,7 +31,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 abstract class DocumentControllerBase extends AdminController implements EventedControllerInterface
 {
-    use Model\Element\AdminStyleTrait;
+    use AdminStyleTrait;
 
     /**
      * @param Request $request

redcar1995/Shopping-site

commit 0e91f65a120685cc84342a215ba994f74772f77d

Fixed namespace issues caused by 4714c71958904ebd7b9209574856d2c6c8e8763c

bundles/AdminBundle/Controller/Traits/AdminStyleTrait.php
@@ -17,6 +17,9 @@
 
 namespace Pimcore\Bundle\AdminBundle\Controller\Traits;
 
+use Pimcore\Model\Element\ElementInterface;
+use Pimcore\Model\Element\Service;
+
 trait AdminStyleTrait
 {
     /**