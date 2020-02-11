redcar1995/Shopping-site

commit a5184dcfe5aa782bef218331bdb57b2ce09ff893

[Data Object] Custom URL-Slug for Data Objects (#5693)
* [Data Object] Custom URL-Slug for Data Objects #5575

* [Data Object] Custom URL-Slug for Data Objects #5575

* [Data Object] Custom URL-Slug for Data Objects #5575 # added classId. added site and class cleanup

* [Data Object] Custom URL-Slug for Data Objects #5575 - use class id in resolver

* [Data Object] Custom URL-Slug for Data Objects #5575 - delete

* [Data Object] Custom URL-Slug for Data Objects #5575 - class definition - site selection

* [Data Object] Custom URL-Slug for Data Objects #5575 - delete

* [Data Object] Custom URL-Slug for Data Objects #5575 - edit UI

* [Data Object] Custom URL-Slug for Data Objects #5575 - fallback site

* Added basic routing (not finished yet!)

* [Data Object] Custom URL-Slug for Data Objects #5575 - validation

* Finished routing + docs

* Coding Standards Fixer Bot

* Removed 'paymentID' from list of required fields (#5704)

* Remove calls to $fielddefinition->isRemoteOwner (#5703)

* [Data Object] Custom URL-Slug for Data Objects #5575

* [Data Object] Custom URL-Slug for Data Objects #5575 - lazy loading

* [Data Object] Custom URL-Slug for Data Objects #5575 - csv import/export

* [Data Object] Custom URL-Slug for Data Objects #5575 - csv import/export

* [Data Object] Custom URL-Slug for Data Objects #5575 - experimental note

* notice

* some small changes

* some small changes

* fixed SQL

* bug fix

* bug fix

* bug fix

Co-authored-by: Josef Aichhorn <josef.aichhorn@elements.at>
Co-authored-by: BlackbitNeueMedien <dev@blackbit.de>

lib/Routing/Dynamic/DataObjectRouteHandler.php
@@ -1,0 +1,139 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Pimcore\Routing\Dynamic;
+
+use Pimcore\Config;
+use Pimcore\Controller\Config\ConfigNormalizer;
+use Pimcore\Db\ConnectionInterface;
+use Pimcore\Http\Request\Resolver\SiteResolver;
+use Pimcore\Http\RequestHelper;
+use Pimcore\Model\DataObject;
+use Pimcore\Model\Document;
+use Pimcore\Routing\DataObjectRoute;
+use Pimcore\Routing\DocumentRoute;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Symfony\Component\Routing\RouteCollection;
+
+class DataObjectRouteHandler implements DynamicRouteHandlerInterface
+{
+    /**
+     * @var Document\Service
+     */
+    private $documentService;
+
+    /**
+     * @var SiteResolver
+     */
+    private $siteResolver;
+
+    /**
+     * @var RequestHelper
+     */
+    private $requestHelper;
+
+    /**
+     * @var ConfigNormalizer
+     */
+    private $configNormalizer;
+
+
+    /**
+     * @var ConnectionInterface
+     */
+    private $db;
+
+    /**
+     * @param Document\Service $documentService
+     * @param SiteResolver $siteResolver
+     * @param RequestHelper $requestHelper
+     * @param ConfigNormalizer $configNormalizer
+     * @param ConnectionInterface $db
+     */
+    public function __construct(
+        Document\Service $documentService,
+        SiteResolver $siteResolver,
+        RequestHelper $requestHelper,
+        ConfigNormalizer $configNormalizer,
+        ConnectionInterface $db
+    ) {
+        $this->documentService = $documentService;
+        $this->siteResolver = $siteResolver;
+        $this->requestHelper = $requestHelper;
+        $this->configNormalizer = $configNormalizer;
+        $this->db = $db;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRouteByName(string $name)
+    {
+        if (preg_match('/^data_object_(\d+)_(.*)$/', $name, $match)) {
+            $slug = DataObject\Data\UrlSlug::resolveSlug($match[2]);
+            if($slug && $slug->getObjectId() == $match[1]) {
+                /** @var DataObject\Concrete $object **/
+                $object = DataObject::getById($match[1]);
+                if($object instanceof DataObject\Concrete && $object->isPublished()) {
+                    return $this->buildRouteForFromSlug($slug, $object);
+                }
+            }
+        }
+
+        throw new RouteNotFoundException(sprintf("Route for name '%s' was not found", $name));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function matchRequest(RouteCollection $collection, DynamicRequestContext $context)
+    {
+
+        $siteIds = [0];
+        $site = $this->siteResolver->getSite($context->getRequest());
+        if($site) {
+            $siteIds[] = $site->getId();
+        }
+
+        $slug = DataObject\Data\UrlSlug::resolveSlug($context->getPath());
+        if($slug) {
+            $object = DataObject::getById($slug->getObjectId());
+            if($object instanceof DataObject\Concrete && $object->isPublished()) {
+                $route = $this->buildRouteForFromSlug($slug, $object);
+                $collection->add($route->getRouteKey(), $route);
+            }
+        }
+    }
+
+    /**
+     * @param DataObject\Data\UrlSlug $slug
+     * @param DataObject\Concrete $object
+     * @return DataObjectRoute
+     * @throws \Exception
+     */
+    private function buildRouteForFromSlug(DataObject\Data\UrlSlug $slug, DataObject\Concrete $object): DataObjectRoute
+    {
+        $route = new DataObjectRoute($slug->getSlug());
+        $route->setOption('utf8', true);
+        $route->setObject($object);
+        $route->setSlug($slug);
+        $route->setDefault('_controller', $slug->getAction());
+        $route->setDefault('object', $object);
+        $route->setDefault('urlSlug', $slug);
+        return $route;
+    }
+}

bundles/AdminBundle/Controller/Admin/DataObject/DataObjectController.php
@@ -553,7 +553,7 @@ class DataObjectController extends ElementControllerBase implements EventedContr
         $parent = DataObject\Service::hasInheritableParentObject($object);
         $getter = 'get' . ucfirst($key);
 
-        // Optimization for lazy loaded relations (note that this is just for AbstractRelations, not for all
+        // Editmode optimization for lazy loaded relations (note that this is just for AbstractRelations, not for all
         // LazyLoadingSupportInterface types. It tries to optimize fetching the data needed for the editmode without
         // loading the entire target element.
         // ReverseManyToManyObjectRelation should go in there anyway (regardless if it a version or not),

bundles/AdminBundle/Controller/Admin/SettingsController.php
@@ -1092,15 +1092,20 @@ class SettingsController extends AdminController
      */
     public function getAvailableSitesAction(Request $request)
     {
+        $excludeMainSite = $request->get('excludeMainSite');
+
         $sitesList = new Model\Site\Listing();
         $sitesObjects = $sitesList->load();
-        $sites = [[
-            'id' => 'default',
-            'rootId' => 1,
-            'domains' => '',
-            'rootPath' => '/',
-            'domain' => $this->trans('main_site')
-        ]];
+        $sites = [];
+        if (!$excludeMainSite) {
+            $sites[] = [
+                'id' => 'default',
+                'rootId' => 1,
+                'domains' => '',
+                'rootPath' => '/',
+                'domain' => $this->trans('main_site')
+            ];
+        }
 
         foreach ($sitesObjects as $site) {
             if ($site->getRootDocument()) {

bundles/AdminBundle/Helper/GridHelperService.php
@@ -36,10 +36,12 @@ class GridHelperService
      *
      * @return array
      */
-    public function getFeatureFilters(string $filterJson, ClassDefinition $class, string $requestedLanguage): array
+    public function getFeatureAndSlugFilters(string $filterJson, ClassDefinition $class, string $requestedLanguage): array
     {
-        $joins = [];
-        $conditions = [];
+        $featureJoins = [];
+        $slugJoins = [];
+        $slugConditions = [];
+        $featureConditions = [];
 
         if ($filterJson) {
             $filters = json_decode($filterJson, true);
@@ -77,52 +79,84 @@ class GridHelperService
 
                 $keyParts = explode('~', $filterField);
 
-                if (substr($filterField, 0, 1) != '~') {
-                    continue;
+                $slugFd = null;
+                $field = null;
+                $slugKey = null;
+                $mappedKey = null;
+
+                if (substr($filterField, 0, 1) == '~') {
+                    $type = $keyParts[1];
+                    if ($type != 'classificationstore') {
+                        continue;
+                    }
+
+                    $fieldName = $keyParts[2];
+                    $groupKeyId = explode('-', $keyParts[3]);
+
+                    /** @var Model\DataObject\ClassDefinition\Data\Classificationstore $csFieldDefinition */
+                    $csFieldDefinition = $class->getFieldDefinition($fieldName);
+
+                    $language = $requestedLanguage;
+                    if (!$csFieldDefinition->isLocalized()) {
+                        $language = 'default';
+                    }
+
+                    $groupId = $groupKeyId[0];
+                    $keyid = $groupKeyId[1];
+
+                    $keyConfig = Model\DataObject\Classificationstore\KeyConfig::getById($keyid);
+                    $type = $keyConfig->getType();
+                    $definition = json_decode($keyConfig->getDefinition());
+                    $field = \Pimcore\Model\DataObject\Classificationstore\Service::getFieldDefinitionFromJson($definition, $type);
+
+                    if ($field instanceof Model\DataObject\ClassDefinition\Data) {
+                        $mappedKey = 'cskey_' . $fieldName . '_' . $groupId . '_' . $keyid;
+                        $featureJoins[] = ['fieldname' => $fieldName, 'groupId' => $groupId, 'keyId' => $keyid, 'language' => $language];
+                        $featureCondition = $field->getFilterConditionExt(
+                            $filter['value'],
+                            $operator,
+                            [
+                                'name' => $mappedKey]
+                        );
+
+                        $featureConditions[$mappedKey] = $featureCondition;
+                    }
+
+                } elseif (count($keyParts) > 1) {
+                    $brickType = $keyParts[0];
+                    $brickKey = $keyParts[1];
+
+                    $brickDef = Objectbrick\Definition::getByKey($brickType);
+                    if ($slugFd = $brickDef->getFieldDefinition($brickKey) instanceof ClassDefinition\Data\UrlSlug) {
+                        $slugKey = $brickKey;
+                        $slugJoins[] = ['fieldname' => $brickKey];
+                    }
+                } else {
+                    if ($slugFd = $class->getFieldDefinition($filterField) instanceof ClassDefinition\Data\UrlSlug) {
+                        $slugKey = $filterField;
+                        $slugJoins[] = ['fieldname' => $filterField];
+
+                    }
                 }
 
-                $type = $keyParts[1];
-                if ($type != 'classificationstore') {
-                    continue;
-                }
-
-                $fieldName = $keyParts[2];
-                $groupKeyId = explode('-', $keyParts[3]);
-
-                /** @var Model\DataObject\ClassDefinition\Data\Classificationstore $csFieldDefinition */
-                $csFieldDefinition = $class->getFieldDefinition($fieldName);
-
-                $language = $requestedLanguage;
-                if (!$csFieldDefinition->isLocalized()) {
-                    $language = 'default';
-                }
-
-                $groupId = $groupKeyId[0];
-                $keyid = $groupKeyId[1];
-
-                $keyConfig = Model\DataObject\Classificationstore\KeyConfig::getById($keyid);
-                $type = $keyConfig->getType();
-                $definition = json_decode($keyConfig->getDefinition());
-                $field = \Pimcore\Model\DataObject\Classificationstore\Service::getFieldDefinitionFromJson($definition, $type);
-
-                if ($field instanceof Model\DataObject\ClassDefinition\Data) {
-                    $mappedKey = 'cskey_' . $fieldName . '_' . $groupId . '_' . $keyid;
-                    $joins[] = ['fieldname' => $fieldName, 'groupId' => $groupId, 'keyId' => $keyid, 'language' => $language];
-                    $condition = $field->getFilterConditionExt(
+                if ($field && $slugFd) {
+                    $slugCondition = $field->getFilterConditionExt(
                         $filter['value'],
                         $operator,
                         [
-                            'name' => $mappedKey]
+                            'name' => $slugKey]
                     );
 
-                    $conditions[$mappedKey] = $condition;
+                    $slugConditions[$mappedKey] = $slugCondition;
                 }
             }
         }
 
         $result = [
-            'joins' => $joins,
-            'conditions' => $conditions
+            'featureJoins' => $featureJoins,
+            'slugJoins' => $slugJoins,
+            'featureConditions' => $featureConditions,
+            'slugConditions' => $slugConditions
         ];
 
         return $result;
@@ -180,6 +214,7 @@ class GridHelperService
 
                 $field = $class->getFieldDefinition($filterField);
                 $brickField = null;
+                $brickKey = null;
                 $brickType = null;
                 $brickDescriptor = null;
                 if (!$field) {
@@ -241,7 +276,11 @@ class GridHelperService
                         if ($brickDescriptor) {
                             $brickPrefix = $db->quoteIdentifier($brickType . '_localized') . '.';
                         } else {
-                            $brickPrefix = $db->quoteIdentifier($brickType) . '.';
+                            if ($brickField instanceof ClassDefinition\Data\UrlSlug) {
+                                $brickPrefix = $db->quoteIdentifier($brickKey) . '.';
+                            } else {
+                                $brickPrefix = $db->quoteIdentifier($brickType) . '.';
+                            }
                         }
                     }
                     if (is_array($filter['value'])) {
@@ -256,6 +295,8 @@ class GridHelperService
                         $conditionPartsFilters[] = $brickPrefix . $brickField->getFilterCondition($filter['value'], $operator,
                                 ['brickType' => $brickType]);
                     }
+                } elseif ($field instanceof ClassDefinition\Data\UrlSlug) {
+                    $conditionPartsFilters[] = $db->quoteIdentifier($field->getName()) . "." . $field->getFilterCondition($filter['value'], $operator);;
                 } elseif ($field instanceof ClassDefinition\Data) {
                     // custom field
                     if (is_array($filter['value'])) {
@@ -338,13 +379,13 @@ class GridHelperService
      * @param DataObject\Listing\Concrete $list
      * @param array $featureJoins
      * @param ClassDefinition $class
-     * @param array $featureFilters
+     * @param array $featureAndSlugFilters
      */
-    public function addGridFeatureJoins(DataObject\Listing\Concrete $list, array $featureJoins, ClassDefinition $class, array $featureFilters)
+    public function addGridFeatureJoins(DataObject\Listing\Concrete $list, array $featureJoins, ClassDefinition $class, array $featureAndSlugFilters)
     {
         if ($featureJoins) {
             $me = $list;
-            $list->onCreateQuery(function (Db\ZendCompatibility\QueryBuilder $select) use ($featureJoins, $class, $featureFilters, $me) {
+            $list->onCreateQuery(function (Db\ZendCompatibility\QueryBuilder $select) use ($featureJoins, $class, $featureAndSlugFilters, $me) {
                 $db = \Pimcore\Db::get();
 
                 $alreadyJoined = [];
@@ -373,7 +414,51 @@ class GridHelperService
                     );
                 }
 
-                $havings = $featureFilters['conditions'];
+                $havings = $featureAndSlugFilters['featureConditions'];
+                if ($havings) {
+                    $havings = implode(' AND ', $havings);
+                    $select->having($havings);
+                }
+            });
+        }
+    }
+
+    /**
+     * Adds all the query stuff that is needed for displaying, filtering and exporting the slug grid data.
+     *
+     * @param DataObject\Listing\Concrete $list
+     * @param array $slugJoins
+     * @param array $featureAndSlugFilters
+     */
+    public function addSlugJoins(DataObject\Listing\Concrete $list, array $slugJoins, array $featureAndSlugFilters)
+    {
+        if ($slugJoins) {
+            $me = $list;
+            $list->onCreateQuery(function (Db\ZendCompatibility\QueryBuilder $select) use ($slugJoins, $featureAndSlugFilters, $me) {
+                $db = \Pimcore\Db::get();
+
+                $alreadyJoined = [];
+
+                foreach ($slugJoins as $slugJoin) {
+                    $fieldname = $slugJoin['fieldname'];
+
+                    $mappedKey = $fieldname;
+                    $alreadyJoined[$mappedKey] = 1;
+                    $table = $me->getDao()->getTableName();
+                    
+                    $select->joinLeft(
+                        [$mappedKey => 'object_url_slugs'],
+                        '('
+                        . $mappedKey . '.objectId = ' . $table . '.o_id'
+                        . ' and ' . $mappedKey . '.fieldname = ' . $db->quote($fieldname)
+                        . ')',
+                        [
+                            $mappedKey => 'slug'
+                        ]
+                    );
+                }
+
+                $havings = $featureAndSlugFilters['slugConditions'];
                 if ($havings) {
                     $havings = implode(' AND ', $havings);
                     $select->having($havings);
@@ -476,14 +561,16 @@ class GridHelperService
         }
 
         $featureJoins = [];
-        $featureFilters = [];
+        $slugJoins = [];
+        $featureAndSlugFilters = [];
 
         // create filter condition
         if (!empty($requestParams['filter'])) {
             $conditionFilters[] = $this->getFilterCondition($requestParams['filter'], $class);
-            $featureFilters = $this->getFeatureFilters($requestParams['filter'], $class, $requestedLanguage);
-            if ($featureFilters) {
-                $featureJoins = array_merge($featureJoins, $featureFilters['joins']);
+            $featureAndSlugFilters = $this->getFeatureAndSlugFilters($requestParams['filter'], $class, $requestedLanguage);
+            if ($featureAndSlugFilters) {
+                $featureJoins = array_merge($featureJoins, $featureAndSlugFilters['featureJoins']);
+                $slugJoins = array_merge($slugJoins, $featureAndSlugFilters['slugJoins']);
             }
         }
 
@@ -547,7 +634,9 @@ class GridHelperService
             $list->setObjectTypes([DataObject\AbstractObject::OBJECT_TYPE_OBJECT, DataObject\AbstractObject::OBJECT_TYPE_VARIANT]);
         }
 
-        $this->addGridFeatureJoins($list, $featureJoins, $class, $featureFilters);
+        $this->addGridFeatureJoins($list, $featureJoins, $class, $featureAndSlugFilters);
+        $this->addSlugJoins($list, $slugJoins, $featureAndSlugFilters);
+
         $list->setLocale($requestedLanguage);
 
         if (empty($requestParams['filter']) && empty($requestParams['condition']) && empty($requestParams['sort'])) {

bundles/AdminBundle/Resources/public/css/icons.css
@@ -1343,6 +1343,10 @@
     background: url(/bundles/pimcoreadmin/img/flat-color-icons/data_sheet.svg) center center no-repeat !important;
 }
 
+.pimcore_icon_urlSlug {
+    background: url(/bundles/pimcoreadmin/img/twemoji/1f40c.svg) center center no-repeat !important;
+}
+
 .pimcore_icon_input {
     background: url(/bundles/pimcoreadmin/img/flat-color-icons/input.svg) center center no-repeat !important;
 }

bundles/AdminBundle/Resources/public/js/pimcore/object/classes/data/data.js
@@ -79,7 +79,7 @@ pimcore.object.classes.data.data = Class.create({
             }
         });
 
-        var indexCheckbox = new Ext.form.field.Checkbox({
+        this.indexCheckbox = new Ext.form.field.Checkbox({
             fieldLabel: t("index"),
             name: "index",
             itemId: "index",
@@ -88,7 +88,7 @@ pimcore.object.classes.data.data = Class.create({
             hidden: true
         });
 
-        var uniqueCheckbox = new Ext.form.field.Checkbox({
+        this.uniqueCheckbox = new Ext.form.field.Checkbox({
             fieldLabel: t("unique"),
             name: "unique",
             itemId: "unique",
@@ -161,8 +161,8 @@ pimcore.object.classes.data.data = Class.create({
                 disabled: !in_array("tooltip",this.availableSettingsFields)
             },
             this.mandatoryCheckbox,
-            indexCheckbox,
-            uniqueCheckbox,
+            this.indexCheckbox,
+            this.uniqueCheckbox,
             {
                 xtype: "checkbox",
                 fieldLabel: t("not_editable"),
@@ -200,9 +200,9 @@ pimcore.object.classes.data.data = Class.create({
                 disabled: !in_array("visibleSearch",this.availableSettingsFields)
             });
 
-            indexCheckbox.setHidden(false);
+            this.indexCheckbox.setHidden(false);
             if (in_array("unique",this.availableSettingsFields)) {
-                uniqueCheckbox.setHidden(false);
+                this.uniqueCheckbox.setHidden(false);
             }
         }
 

bundles/AdminBundle/Resources/public/js/pimcore/object/classes/data/urlSlug.js
@@ -1,0 +1,141 @@
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+pimcore.registerNS("pimcore.object.classes.data.urlSlug");
+pimcore.object.classes.data.urlSlug = Class.create(pimcore.object.classes.data.data, {
+
+    type: "urlSlug",
+    /**
+     * define where this datatype is allowed
+     */
+    allowIn: {
+        object: true,
+        objectbrick: true,
+        fieldcollection: true,
+        localizedfield: true,
+        classificationstore: true,
+        block: false,
+        encryptedField: false
+    },
+
+    initialize: function (treeNode, initData) {
+        this.type = "urlSlug";
+
+        this.availableSettingsFields = ["name", "title", "tooltip", "mandatory", "noteditable", "invisible",
+            "visibleGridView", "visibleSearch", "style"];
+
+        this.initData(initData);
+
+        this.treeNode = treeNode;
+    },
+
+    getTypeName: function () {
+        return t("url_slug");
+    },
+
+    getGroup: function () {
+        return "other";
+    },
+
+    getIconClass: function () {
+        return "pimcore_icon_urlSlug";
+    },
+
+    getLayout: function ($super) {
+
+        $super();
+
+        this.specificPanel.removeAll();
+        var specificItems = this.getSpecificPanelItems(this.datax);
+        this.specificPanel.add(specificItems);
+
+        return this.layout;
+    },
+
+    getSpecificPanelItems: function (datax) {
+
+        var sitesStore = new Ext.data.JsonStore({
+            autoDestroy: true,
+            autoLoad: true,
+            proxy: {
+                type: 'ajax',
+                url: '/admin/settings/get-available-sites',
+                extraParams: {excludeMainSite: 1}
+            },
+            fields: ['id', 'domain']
+        });
+
+
+        var availableSites = null;
+        if (datax.availableSites) {
+            availableSites = datax.availableSites.join(",");
+        }
+
+        var specificItems = [
+            {
+                xtype: "numberfield",
+                fieldLabel: t("width"),
+                name: "width",
+                value: datax.width
+            },
+            {
+                xtype: "numberfield",
+                fieldLabel: t("domain_label_width"),
+                name: "domainLabelWidth",
+                value: datax.domainLabelWidth
+            }
+            ,
+            {
+                xtype: "textfield",
+                fieldLabel: t("controller_action"),
+                name: "action",
+                value: datax.action,
+                width: 740,
+                disabled: this.isInCustomLayoutEditor()
+            },
+            {
+                xtype: 'container',
+                html: t('url_slug_datatype_info'),
+                style: 'margin-bottom:10px'
+            },
+            new Ext.ux.form.MultiSelect({
+                fieldLabel: t("available_sites"),
+                name: "availableSites",
+                value: availableSites,
+                displayField: "domain",
+                valueField: "id",
+                store: sitesStore,
+                width: 600,
+                disabled: this.isInCustomLayoutEditor()
+            })
+        ];
+
+        return specificItems;
+
+    },
+
+    applySpecialData: function (source) {
+        if (source.datax) {
+            if (!this.datax) {
+                this.datax = {};
+            }
+            Ext.apply(this.datax,
+                {
+                    width: source.datax.width,
+                    action: source.datax.action,
+                    availableSites: source.datax.availableSites,
+                    domainLabelWidth: source.datax.domainLabelWidth
+                });
+        }
+    }
+});

bundles/AdminBundle/Resources/public/js/pimcore/object/helpers/classTree.js
@@ -230,6 +230,8 @@ pimcore.object.helpers.classTree = Class.create({
                 isLeaf = false;
                 draggable = false;
 
+                // create a copy because we have to pop this state
+                brickDescriptor = Ext.clone(brickDescriptor);
                 Ext.apply(brickDescriptor, {
                     insideLocalizedFields: true
                 });

commit 3efb5deeb8888888c29c6d7ae00970db3e0164d8

Injecting ConnectionInterface causes issues when DB connection isn't configured (setup), introduced by #5693 - code was not used anyway ;-)

lib/Routing/Dynamic/DataObjectRouteHandler.php
@@ -18,7 +18,6 @@ declare(strict_types=1);
 namespace Pimcore\Routing\Dynamic;
 
 use Pimcore\Controller\Config\ConfigNormalizer;
-use Pimcore\Db\ConnectionInterface;
 use Pimcore\Http\Request\Resolver\SiteResolver;
 use Pimcore\Http\RequestHelper;
 use Pimcore\Model\DataObject;
@@ -50,29 +49,21 @@ class DataObjectRouteHandler implements DynamicRouteHandlerInterface
     private $configNormalizer;
 
     /**
-     * @var ConnectionInterface
-     */
-    private $db;
-
-    /**
      * @param Document\Service $documentService
      * @param SiteResolver $siteResolver
      * @param RequestHelper $requestHelper
      * @param ConfigNormalizer $configNormalizer
-     * @param ConnectionInterface $db
      */
     public function __construct(
         Document\Service $documentService,
         SiteResolver $siteResolver,
         RequestHelper $requestHelper,
-        ConfigNormalizer $configNormalizer,
-        ConnectionInterface $db
+        ConfigNormalizer $configNormalizer
     ) {
         $this->documentService = $documentService;
         $this->siteResolver = $siteResolver;
         $this->requestHelper = $requestHelper;
         $this->configNormalizer = $configNormalizer;
-        $this->db = $db;
     }
 
     /**