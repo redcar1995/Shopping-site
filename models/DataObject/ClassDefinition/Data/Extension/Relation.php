redcar1995/Shopping-site

commit 191bd0025e5fcfe41df1473461463fe02538ed6e

Fix return types for Relation::getClasses (#6032)
* phpdoc: fix return types for Relation::getClasses

models/DataObject/ClassDefinition/Data/Extension/Relation.php
@@ -14,18 +14,6 @@
 
 namespace Pimcore\Model\DataObject\ClassDefinition\Data\Extension;
 
-/**
- * Class Relation
- *
- * @package Pimcore\Model\DataObject\ClassDefinition\Data\Extension
- *
- * @method bool getDocumentsAllowed()
- * @method bool getAssetsAllowed()
- * @method bool getObjectsAllowed()
- * @method array getDocumentTypes()
- * @method array getAssetTypes()
- * @method array getClasses()
- */
 trait Relation
 {
     /**
@@ -40,7 +28,7 @@ trait Relation
         $strArray = $asArray ? '[]' : '';
 
         // add documents
-        if (method_exists($this, 'getDocumentsAllowed') && $this->getDocumentsAllowed()) {
+        if ($this->getDocumentsAllowed()) {
             $documentTypes = $this->getDocumentTypes();
             if (count($documentTypes) == 0) {
                 $class[] = '\Pimcore\Model\Document\Page' . $strArray;
@@ -54,7 +42,7 @@ trait Relation
         }
 
         // add asset
-        if (method_exists($this, 'getAssetsAllowed') && $this->getAssetsAllowed()) {
+        if ($this->getAssetsAllowed()) {
             $assetTypes = $this->getAssetTypes();
             if (count($assetTypes) == 0) {
                 $class[] = '\Pimcore\Model\Asset' . $strArray;
@@ -67,16 +55,64 @@ trait Relation
 
         // add objects
         if ($this->getObjectsAllowed()) {
-            $classes = $this->getClasses() ? $this->getClasses() : [];
-            if (count($classes) == 0) {
+            $classes = $this->getClasses();
+            if (count($classes) === 0) {
                 $class[] = '\Pimcore\Model\DataObject\AbstractObject' . $strArray;
             } elseif (is_array($classes)) {
-                foreach ($this->getClasses() as $item) {
+                foreach ($classes as $item) {
                     $class[] = sprintf('\Pimcore\Model\DataObject\%s', ucfirst($item['classes']) . $strArray);
                 }
             }
         }
 
         return $class;
+    }
+
+    /**
+     * @return array[
+     *  'classes' => string,
+     * ]
+     */
+    public function getClasses() {
+        return [];
+    }
+
+    /**
+     * @return array[
+     *  'assetTypes' => string,
+     * ]
+     */
+    public function getAssetTypes() {
+        return [];
+    }
+
+    /**
+     * @return array[
+     *  'documentTypes' => string,
+     * ]
+     */
+    public function getDocumentTypes() {
+        return [];
+    }
+
+    /**
+     * @return bool
+     */
+    public function getDocumentsAllowed() {
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getAssetsAllowed() {
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getObjectsAllowed() {
+        return false;
     }
 }

bundles/EcommerceFrameworkBundle/Controller/AdminOrderController.php
@@ -278,7 +278,7 @@ class AdminOrderController extends AdminController implements EventedControllerI
                 $field = $order->getClass()->getFieldDefinition('customer');
                 if ($field instanceof ManyToOneRelation) {
                     $classes = $field->getClasses();
-                    if (count($classes) == 1) {
+                    if (count($classes) === 1) {
                         $class = 'Pimcore\Model\DataObject\\' . reset($classes)['classes'];
                         /* @var \Pimcore\Model\DataObject\Concrete $class */
 

models/DataObject/ClassDefinition/Data/ManyToManyRelation.php
@@ -84,33 +84,33 @@ class ManyToManyRelation extends AbstractRelations implements QueryResourcePersi
      *
      * @var bool
      */
-    public $objectsAllowed;
+    public $objectsAllowed = false;
 
     /**
      *
      * @var bool
      */
-    public $assetsAllowed;
+    public $assetsAllowed = false;
 
     /**
      * Allowed asset types
      *
      * @var array
      */
-    public $assetTypes;
+    public $assetTypes = [];
 
     /**
      *
      * @var bool
      */
-    public $documentsAllowed;
+    public $documentsAllowed = false;
 
     /**
      * Allowed document types
      *
      * @var array
      */
-    public $documentTypes;
+    public $documentTypes = [];
 
     /**
      * @return bool
@@ -157,7 +157,7 @@ class ManyToManyRelation extends AbstractRelations implements QueryResourcePersi
      */
     public function getDocumentTypes()
     {
-        return $this->documentTypes;
+        return $this->documentTypes ?: [];
     }
 
     /**

models/DataObject/ClassDefinition/Data/ManyToOneRelation.php
@@ -75,33 +75,33 @@ class ManyToOneRelation extends AbstractRelations implements QueryResourcePersis
      *
      * @var bool
      */
-    public $objectsAllowed;
+    public $objectsAllowed = false;
 
     /**
      *
      * @var bool
      */
-    public $assetsAllowed;
+    public $assetsAllowed = false;
 
     /**
      * Allowed asset types
      *
      * @var array
      */
-    public $assetTypes;
+    public $assetTypes = [];
 
     /**
      *
      * @var bool
      */
-    public $documentsAllowed;
+    public $documentsAllowed = false;
 
     /**
      * Allowed document types
      *
      * @var array
      */
-    public $documentTypes;
+    public $documentTypes = [];
 
     /**
      * @return bool
@@ -148,7 +148,7 @@ class ManyToOneRelation extends AbstractRelations implements QueryResourcePersis
      */
     public function getDocumentTypes()
     {
-        return $this->documentTypes;
+        return $this->documentTypes ?: [];
     }
 
     /**
@@ -190,7 +190,7 @@ class ManyToOneRelation extends AbstractRelations implements QueryResourcePersis
      */
     public function getAssetTypes()
     {
-        return $this->assetTypes;
+        return $this->assetTypes ?: [];
     }
 
     /**

models/DataObject/ClassDefinition/Data/Relations/AbstractRelations.php
@@ -37,7 +37,7 @@ abstract class AbstractRelations extends Data implements
      *
      * @var array
      */
-    public $classes;
+    public $classes = [];
 
     /** Optional path formatter class
      * @var null|string
@@ -45,11 +45,13 @@ abstract class AbstractRelations extends Data implements
     public $pathFormatterClass;
 
     /**
-     * @return array
+     * @return array[
+     *  'classes' => string,
+     * ]
      */
     public function getClasses()
     {
-        return $this->classes;
+        return $this->classes ?: [];
     }
 
     /**

models/DataObject/ClassDefinition/Data/Relations/AllowObjectRelationTrait.php
@@ -20,7 +20,7 @@ trait AllowObjectRelationTrait
         $allowed = true;
         if (!$this->getObjectsAllowed()) {
             $allowed = false;
-        } elseif ($this->getObjectsAllowed() and is_array($allowedClasses) and count($allowedClasses) > 0) {
+        } elseif ($this->getObjectsAllowed() and count($allowedClasses) > 0) {
             $allowedClassnames = [];
             foreach ($allowedClasses as $c) {
                 $allowedClassnames[] = $c['classes'];

commit 1f248a1e35ba2fb475d6e50ec80a0c932583241b

[DataObjects] Relations - fix getClasses() return value - regression caused by #6032

models/DataObject/ClassDefinition/Data/Extension/Relation.php
@@ -75,7 +75,7 @@ trait Relation
      */
     public function getClasses()
     {
-        return [];
+        return $this->classes ?: [];
     }
 
     /**