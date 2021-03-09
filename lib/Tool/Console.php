redcar1995/Shopping-site

commit d63f4a119171136a5a31e73c988f045df488abf4

Revert execInBackground (#8371)
* Revert execInBackground

* fix phpdoc

* Added upgrade notes

* Use proper escaping for commands

* Revert background option from runPhpScript()

Co-authored-by: Bernhard Rusch <bernhard.rusch@elements.at>
Co-authored-by: Divesh <divesh.pahuja@pimcore.com>

lib/Tool/Console.php
@@ -273,11 +273,10 @@ class Console
      * @param string|array $arguments
      * @param string|null $outputFile
      * @param int|null $timeout
-     * @param bool $background
      *
-     * @return string|int
+     * @return string
      */
-    public static function runPhpScript($script, $arguments = '', $outputFile = null, $timeout = null, $background = false)
+    public static function runPhpScript($script, $arguments = '', $outputFile = null, $timeout = null)
     {
         $cmd = self::buildPhpScriptCmd($script, $arguments);
         self::addLowProcessPriority($cmd);
@@ -289,28 +288,32 @@ class Console
 
         if (!empty($outputFile)) {
             $logHandle = fopen($outputFile, 'a');
-            $exitCode = $process->wait(function ($type, $buffer) use ($logHandle) {
+            $process->wait(function ($type, $buffer) use ($logHandle) {
                 fwrite($logHandle, $buffer);
             });
             fclose($logHandle);
         } else {
-            $exitCode = $process->wait();
+            $process->wait();
         }
 
-        return $background ? $exitCode : $process->getOutput();
+        return $process->getOutput();
     }
 
     /**
-     * @deprecated since v6.9. Use runPhpScript instead.
+     * @deprecated since v6.9. For long running background tasks switch to a queue implementation.
+     *
      * @param string $script
-     * @param string $arguments
+     * @param string|array $arguments
      * @param string|null $outputFile
      *
      * @return int
      */
     public static function runPhpScriptInBackground($script, $arguments = '', $outputFile = null)
     {
-        return self::runPhpScript($script, $arguments, $outputFile);
+        $cmd = self::buildPhpScriptCmd($script, $arguments);
+        $process = new Process($cmd);
+        $commandLine = $process->getCommandLine();
+        return self::execInBackground($commandLine, $outputFile);
     }
 
     /**
@@ -361,8 +364,7 @@ class Console
     }
 
     /**
-     *
-     * @deprecated since v.6.9. Use Symfony\Component\Process\Process instead.
+     * @deprecated since v.6.9. Use Symfony\Component\Process\Process instead. For long running background tasks use queues.
      * @static
      *
      * @param string $cmd
@@ -372,7 +374,6 @@ class Console
      */
     public static function execInBackground($cmd, $outputFile = null)
     {
-
         // windows systems
         if (self::getSystemEnvironment() == 'windows') {
             return self::execInBackgroundWindows($cmd, $outputFile);
@@ -384,7 +385,7 @@ class Console
     }
 
     /**
-     * @deprecated since v.6.9.
+     * @deprecated since v.6.9. For long running background tasks use queues.
      * @static
      *
      * @param string $cmd
@@ -434,7 +435,7 @@ class Console
     }
 
     /**
-     * @deprecated since v.6.9.
+     * @deprecated since v.6.9. For long running background tasks use queues.
      * @static
      *
      * @param string $cmd

bundles/AdminBundle/Controller/Admin/Document/NewsletterController.php
@@ -364,10 +364,10 @@ class NewsletterController extends DocumentControllerBase
             'progress' => 0,
         ], 'newsletter');
 
-        Console::runPhpScript(
+        Console::runPhpScriptInBackground(
             realpath(PIMCORE_PROJECT_ROOT . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'console'),
-            ['internal:newsletter-document-send', $document->getTmpStoreId(), \Pimcore\Tool::getHostUrl()],
-            PIMCORE_LOG_DIRECTORY . DIRECTORY_SEPARATOR . 'newsletter-sending-output.log', null, true
+            'internal:newsletter-document-send ' . escapeshellarg($document->getTmpStoreId()) . ' ' . escapeshellarg(\Pimcore\Tool::getHostUrl()),
+            PIMCORE_LOG_DIRECTORY . DIRECTORY_SEPARATOR . 'newsletter-sending-output.log'
         );
 
         return $this->adminJson(['success' => true]);

lib/Web2Print/Processor.php
@@ -25,7 +25,6 @@ use Pimcore\Web2Print\Processor\PdfReactor;
 use Pimcore\Web2Print\Processor\WkHtmlToPdf;
 use Symfony\Component\Lock\Factory as LockFactory;
 use Symfony\Component\Lock\LockInterface;
-use Symfony\Component\Process\Process;
 
 abstract class Processor
 {
@@ -75,26 +74,20 @@ abstract class Processor
         $this->saveJobConfigObjectFile($jobConfig);
         $this->updateStatus($documentId, 0, 'prepare_pdf_generation');
 
-        $args = ['-p', $jobConfig->documentId];
+        $args = ['-p ' . $jobConfig->documentId];
 
         $env = \Pimcore\Config::getEnvironment();
         if ($env !== false) {
-            array_push($args, '--env=' . $env);
+            $args[] = '--env=' . $env;
         }
 
-        $cmd = array_merge([Tool\Console::getPhpCli(), realpath(PIMCORE_PROJECT_ROOT . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'console'), 'pimcore:web2print:pdf-creation'], $args);
-        Tool\Console::addLowProcessPriority($cmd);
-        $process = new Process($cmd);
-        Logger::info($process->getCommandLine());
+        $cmd = Tool\Console::getPhpCli() . ' ' . realpath(PIMCORE_PROJECT_ROOT . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'console'). ' pimcore:web2print:pdf-creation ' . implode(' ', $args);
+
+        Logger::info($cmd);
         $disableBackgroundExecution = $config['disableBackgroundExecution'] ?? false;
 
         if (!$disableBackgroundExecution) {
-            $process->start();
-            $logHandle = fopen(PIMCORE_LOG_DIRECTORY . DIRECTORY_SEPARATOR . 'web2print-output.log', 'a');
-            $process->wait(function ($type, $buffer) use ($logHandle) {
-                fwrite($logHandle, $buffer);
-            });
-            fclose($logHandle);
+            Tool\Console::execInBackground($cmd, PIMCORE_LOG_DIRECTORY . DIRECTORY_SEPARATOR . 'web2print-output.log');
 
             return true;
         }

models/Asset/Video/Thumbnail/Processor.php
@@ -251,7 +251,7 @@ class Processor
     public function convert()
     {
         $this->save();
-        Console::runPhpScript(realpath(PIMCORE_PROJECT_ROOT . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'console'), ['internal:video-converter', $this->getProcessId()], null, null, true);
+        Console::runPhpScriptInBackground(realpath(PIMCORE_PROJECT_ROOT . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'console'), 'internal:video-converter ' . $this->getProcessId());
     }
 
     /**

commit d456e8a08775d878a1c8fb0e9c9e1c8ee5bd8e22

merge fixed caused by #8371

lib/Tool/Console.php
@@ -15,6 +15,7 @@
 namespace Pimcore\Tool;
 
 use Pimcore\Config;
+use Pimcore\Logger;
 use Symfony\Component\Process\ExecutableFinder;
 use Symfony\Component\Process\Process;
 
@@ -34,6 +35,27 @@ class Console
      * @var array
      */
     protected static $executableCache = [];
+
+    /**
+     * @deprecated since v.6.9.
+     * @static
+     *
+     * @return string "windows" or "unix"
+     */
+    public static function getSystemEnvironment()
+    {
+        if (self::$systemEnvironment == null) {
+            if (stripos(php_uname('s'), 'windows') !== false) {
+                self::$systemEnvironment = 'windows';
+            } elseif (stripos(php_uname('s'), 'darwin') !== false) {
+                self::$systemEnvironment = 'darwin';
+            } else {
+                self::$systemEnvironment = 'unix';
+            }
+        }
+
+        return self::$systemEnvironment;
+    }
 
     /**
      * @param string $name